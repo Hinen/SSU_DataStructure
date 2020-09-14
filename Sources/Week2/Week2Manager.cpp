d3dbe4956d7979bf1f7d7129380787564e2cfac8
Hinen <qkrqldtjqj@naver.com>
2020-09-14 22:29:54 +0900

    Create Week2Manager

---
+++ Sources/Week2/Week2Manager.cpp
@@ -0,0 +1,16 @@
+#include "Week2Manager.h"
+
+Week2Manager::Week2Manager() : WeekManagerBase(2)
+{
+
+}
+
+Week2Manager::~Week2Manager()
+{
+
+}
+
+void Week2Manager::Main()
+{
+	WeekManagerBase::Main();
+}
---
+++ Sources/Week2/Week2Manager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../WeekManagerBase.h"
+
+class Week2Manager : public WeekManagerBase
+{
+public:
+	Week2Manager();
+	~Week2Manager();
+
+public:
+	virtual void Main() override;
+
+private:
+};
