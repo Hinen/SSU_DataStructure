ee33eb6ce9b55a55c46f2731624ac2fc1ec65b27
Hinen <qkrqldtjqj@naver.com>
2020-09-15 21:34:08 +0900

    Fix Poly I/O C -> C++

--- Sources/Week2/Polynomial.h
+++ Sources/Week2/Polynomial.h
@@ -18,22 +18,22 @@ private:
 public:
 	void Read()
 	{
-		printf("다항식의 최고 차수를 입력하시오: ");
-		scanf("%d", &_degree);
+		cout << "다항식의 최고 차수를 입력하시오: ";
+		cin >> _degree;
 
-		printf("각 항의 계수를 입력하시오 (총 %d개): ", _degree + 1);
+		cout << "각 항의 계수를 입력하시오 (총 " << _degree + 1 << "개): ";
 		for (int i = 0; i <= _degree; i++)
-			scanf("%f", _coef + i);
+			cin >> _coef[i];
 	}
 
 	void Display(char *str = " Poly = ")
 	{
-		printf("\t%s", str);
+		cout << "\t" << str;
 
 		for (int i = 0; i < _degree; i++)
-			printf("%5.1f x^%d + ", _coef[i], _degree - i);
+			cout << _coef[i] << " x^" << _degree - i << " + ";
 
-		printf("%4.1f\n", _coef[_degree]);
+		cout << _coef[_degree] << endl;
 	}
 
 	void Add(Polynomial a, Polynomial b)
