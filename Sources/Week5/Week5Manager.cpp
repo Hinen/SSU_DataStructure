ac2439cbb21cdf87d6b275ba3aceb43f63d798af
Hinen <qkrqldtjqj@naver.com>
2020-10-07 01:52:37 +0900

    FibonacciNumbers Using Queue

--- Sources/Week5/Week5Manager.cpp
+++ Sources/Week5/Week5Manager.cpp
@@ -13,4 +13,40 @@ Week5Manager::~Week5Manager()
 void Week5Manager::Main()
 {
 	WeekManagerBase::Main();
+
+	FibonacciNumbers();
+}
+
+void Week5Manager::FibonacciNumbers()
+{
+	int count = 0;
+
+	cout << "피보나치 수열 항의 갯수를 입력하세요 : ";
+	cin >> count;
+
+	if (count <= 0)
+	{
+		cout << "항의 갯수는 0보다 커야합니다." << endl;
+		return;
+	}
+
+	queue<int> queue;
+	for (int i = 0; i < count; i++)
+	{
+		if (i < 2)
+		{
+			queue.push(1);
+			cout << queue.back() << " ";
+
+			continue;
+		}
+
+		int num1 = queue.front();
+		int num2 = queue.back();
+
+		queue.pop();
+
+		queue.push(num1 + num2);
+		cout << queue.back() << " ";
+	}
 }
--- Sources/Week5/Week5Manager.h
+++ Sources/Week5/Week5Manager.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../WeekManagerBase.h"
+#include <queue>
 
 class Week5Manager : public WeekManagerBase
 {
@@ -10,4 +11,7 @@ public:
 
 public:
 	virtual void Main() override;
+
+private:
+	void FibonacciNumbers();
 };
