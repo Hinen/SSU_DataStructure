ff935dcce241221f5af00deabdf812516062a1a0
Hinen <qkrqldtjqj@naver.com>
2020-10-07 02:14:24 +0900

    MazeSearch using queue

--- Sources/Week5/Week5Manager.cpp
+++ Sources/Week5/Week5Manager.cpp
@@ -14,7 +14,8 @@ void Week5Manager::Main()
 {
 	WeekManagerBase::Main();
 
-	FibonacciNumbers();
+	//FibonacciNumbers();
+	MazeSearch();
 }
 
 void Week5Manager::FibonacciNumbers()
@@ -49,4 +50,75 @@ void Week5Manager::FibonacciNumbers()
 		queue.push(num1 + num2);
 		cout << queue.back() << " ";
 	}
+}
+
+void Week5Manager::MazeSearch()
+{
+	int startPosX = -1;
+	int startPosY = -1;
+
+	for (int i = 0; i < mazeSize; i++)
+	{
+		for (int j = 0; j < mazeSize; j++)
+		{
+			if (map[i][j] == 'e')
+			{
+				startPosX = i;
+				startPosY = j;
+			}
+		}
+	}
+
+	if (startPosX == -1 || startPosY == -1)
+	{
+		cout << "map에 시작 위치(e)가 찍혀있지 않습니다." << endl;
+		return;
+	}
+
+	queue<Location2D> locQueue;
+	locQueue.push(Location2D(startPosX, startPosY));
+
+	bool isSuccessFindExit = false;
+	while (locQueue.empty() == false)
+	{
+		Location2D here = locQueue.front();
+		locQueue.pop();
+
+		int r = here.row;
+		int c = here.col;
+
+		cout << "(" << r << "," << c << ")" << endl;
+
+		if (map[r][c] == 'x')
+		{
+			isSuccessFindExit = true;
+			break;
+		}
+		else
+		{
+			if (IsValidLocation(r - 1, c))
+				locQueue.push(Location2D(r - 1, c));
+
+			if (IsValidLocation(r + 1, c))
+				locQueue.push(Location2D(r + 1, c));
+
+			if (IsValidLocation(r, c - 1))
+				locQueue.push(Location2D(r, c - 1));
+
+			if (IsValidLocation(r, c + 1))
+				locQueue.push(Location2D(r, c + 1));
+
+			map[r][c] = '.';
+		}
+	}
+
+	cout << "미로 탐색 " << (isSuccessFindExit ? "성공!" : "실패.. (이 미로는 출구가 존재하지 않습니다.)") << endl;
+}
+
+bool Week5Manager::IsValidLocation(int r, int c)
+{
+	if (r < 0 || c < 0 || r >= mazeSize || c >= mazeSize)
+		return false;
+	else
+		return map[r][c] == '0' || map[r][c] == 'x';
 }
--- Sources/Week5/Week5Manager.h
+++ Sources/Week5/Week5Manager.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "../WeekManagerBase.h"
+#include "../Week3/Location2D.h"
+#include "../Week3/MazeMap.h"
 #include <queue>
 
 class Week5Manager : public WeekManagerBase
@@ -14,4 +16,6 @@ public:
 
 private:
 	void FibonacciNumbers();
+	void MazeSearch();
+	bool IsValidLocation(int r, int c);
 };
